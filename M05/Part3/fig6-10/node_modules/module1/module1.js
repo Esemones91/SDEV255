1f413ae91dc8f8a999cacd7bfa14830a6bc3b7b9
Esemones91 <eeesemones@gmail.com>
2024-11-24 15:10:21 -0500

    M05 Ch6 11/24
    Node.js asgnmnts from M05 Ch6

---
+++ M05/Part3/fig6-10/node_modules/module1/module1.js
@@ -0,0 +1,13 @@
+console.log("module1 is loaded");
+function add(a, b) {
+  return a+b;
+}
+function mult(a, b) {
+  return a*b;
+}
+// first define the main function
+module.exports = add;  // the add() function defined outside 
+                       // the module, is made main
+// then define the secondary functions
+module.exports.mult = mult;   // and the mult() function 
+                              // becomes usable as well
