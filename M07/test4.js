984b1af428b9a0a634e6975d182f09fbbead8549
Esemones91 <eeesemones@gmail.com>
2024-12-06 14:21:54 -0500

    M07 Ch8 Pt1 12/8 v2
    JS asgnmnts for M07 Ch8 Pt1 with added test files

---
+++ M07/test4.js
@@ -0,0 +1,12 @@
+// Using open and error events on database connection (test.js file)
+
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/mydb_test");
+mongoose.connection.on("error", function() {
+ console.log("mydb_test database connection error")
+});
+mongoose.connection.on("open", function() {
+ console.log("Successful connection to mydb_test 
+ database");
+});
+console.log("Connecting to mydb_test database in progress...");
